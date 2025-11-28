49951328d38ffa2ad5fb782bec2d39616edb841f
Arthur_stepanov <Arthur_stepanov06@mail.ru>
2025-11-28 17:01:17 +0900

    feat cathedrals

---
+++ svfu_vvs_sar1.sql
@@ -0,0 +1,14 @@
+SELECT * FROM cathedral
+
+UPDATE groups
+set cathedrial_id = 1
+WHERE groups.year = 2025
+
+UPDATE groups
+set cathedrial_id = 2
+WHERE groups.shot_name = 'Б-ПИ-25'
+
+SELECT * FROM groups;
+
+SELECT * FROM groups
+WHERE groups.cathedrial_id = '1';
